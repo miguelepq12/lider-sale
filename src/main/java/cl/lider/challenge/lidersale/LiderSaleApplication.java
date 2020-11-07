daf21ede5b8952e7851f6320efb3a27b127f4472
Miguel Piña <miguelepq@gmail.com>
2020-11-07 02:24:11 -0300

    init

---
+++ src/main/java/cl/lider/challenge/lidersale/LiderSaleApplication.java
@@ -0,0 +1,13 @@
+package cl.lider.challenge.lidersale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiderSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiderSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/lider/challenge/lidersale/LiderSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.lider.challenge.lidersale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiderSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
