092468f75adf1ab1ccc598bd86e4af7bb05067c7
Miguel Piña <miguelepq@gmail.com>
2020-11-11 09:39:57 -0300

    clean code #2

--- src/test/java/cl/lider/challenge/lidersale/entity/product/ValidateProductTest.java
+++ src/test/java/cl/lider/challenge/lidersale/entity/product/ValidateProductTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class ValidateProductTest {
+class ValidateProductTest {
 
     static ValidateProduct validate;
 
